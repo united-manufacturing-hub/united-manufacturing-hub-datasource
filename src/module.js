281514d89ec6bd0cc7797285aa66d3356f65961a
Ferdinand Linnenberg <fl@umh.app>
2021-07-27 15:20:36 +0200

    fix: make yarn happy

--- src/module.js
+++ src/module.js
@@ -1,7 +1,8 @@
 import { DataSourcePlugin } from '@grafana/data';
-import { DataSource } from './DataSource';
+//import { DataSource } from './DataSource';
 import { ConfigEditor } from './ConfigEditor';
 import { QueryEditor } from './QueryEditor';
+var DataSource = require("DataSource");
 export var plugin = new DataSourcePlugin(DataSource)
     .setConfigEditor(ConfigEditor)
     .setQueryEditor(QueryEditor);
--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,12 @@
 import { DataSourcePlugin } from '@grafana/data';
-import { DataSource } from './DataSource';
+//import { DataSource } from './DataSource';
 import { ConfigEditor } from './ConfigEditor';
 import { QueryEditor } from './QueryEditor';
 import { JSONQuery, JSONQueryOptions } from './types';
 
-export const plugin = new DataSourcePlugin<DataSource, JSONQuery, JSONQueryOptions>(DataSource)
+const DataSource = require("DataSource")
+
+export const plugin = new DataSourcePlugin<typeof DataSource, JSONQuery, JSONQueryOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
   .setQueryEditor(QueryEditor);
 
